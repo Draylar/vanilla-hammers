8c97f2fec90e1075fa78cf8886dc74849e260571
Draylar <samuelhimes@gmail.com>
2020-06-24 14:11:32 -0500

    update to 1.16.1 with some cleanups and gradle refactoring

--- src/main/java/com/github/draylar/vh/ModMenuIntegration.java
+++ src/main/java/com/github/draylar/vh/ModMenuIntegration.java
@@ -1,31 +1,17 @@
 package com.github.draylar.vh;
 
 import com.github.draylar.vh.config.VanillaHammersConfig;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.Screen;
-
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
-    public String getModId() {
-        return VanillaHammers.MODID;
-    }
-
-    @Override
-    public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
-        return Optional.of(AutoConfig.getConfigScreen(VanillaHammersConfig.class, screen));
-    }
-
-    @Override
-    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return screen -> AutoConfig.getConfigScreen(VanillaHammersConfig.class, screen).get();
     }
 }
